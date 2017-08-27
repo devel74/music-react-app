3d08522c9ece7b1619e8ef19b1e213010a68c70e
R.Lukyanchikov <ruslan.lukyanchikov74@gmail.com>
2017-08-27 22:01:52 +0500

    Global constants added

---
+++ app/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_SERVER = 'http://ws.audioscrobbler.com/2.0/';
+export const AUTH_PARAMS = {
+  API_KEY: 'd8b328133bea69186a571cffa64366a9',
+  SHARED_SECRET: 'b7e672bee2d56d3d4b208be608b41b3f',
+  AUTH_URL: 'http://www.last.fm/api/auth/',
+  REDIRECT_URL: 'http://localhost:3000/auth',
+};
